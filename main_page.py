84522548c3984e393d31543f8108f9be179d651b
sarin_beam30 <sarin_beam30@hotmail.com>
2019-05-31 15:36:23 +0700

    Commit fifth time
    Edit main_page.py && main_page.ui

--- main_page.py
+++ main_page.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'main_page.ui',
 # licensing of 'main_page.ui' applies.
 #
-# Created: Thu May 30 16:41:09 2019
+# Created: Fri May 31 00:09:42 2019
 #      by: pyside2-uic  running on PySide2 5.12.0
 #
 # WARNING! All changes made in this file will be lost!
@@ -11,6 +11,14 @@
 from PySide2 import QtCore, QtGui, QtWidgets
 
 class Ui_main_page(object):
+
+    def save(self):
+        self.get_update_data(self.name_lineEdit.text(), self.email_lineEdit.text(), self.password_lineEdit.text(), self.birth_dateEdit.date().toString('dd-MMM-yyyy'), self.weight_lineEdit.text(), self.height_lineEdit.text(),str(self.sex_comboBox.currentText()))
+
+    def get_update_data(self, name, email, password, birthdate, weight, height, gender):
+        print("Name : " + str(name) + " , Email : " + str(email) + " , Password : " + str(password) + " , Birthdate : " + str(birthdate) + " , Weight : " + str(weight) + " , Height : " + str(height) + " , Gender : " + str(gender))
+        pass
+
     def setupUi(self, main_page):
         main_page.setObjectName("main_page")
         main_page.resize(1600, 900)
@@ -90,12 +98,23 @@ class Ui_main_page(object):
 "border-radius : 15px;\n"
 "background : white;")
         self.food_widgets.setObjectName("food_widgets")
+
+        #QUOTE_WIDGETS
         self.quote_widgets = QtWidgets.QWidget(self.dash_board)
         self.quote_widgets.setGeometry(QtCore.QRect(830, 430, 711, 371))
         self.quote_widgets.setStyleSheet("border : 2px solid black;\n"
 "border-radius : 15px;\n"
 "background : white;")
         self.quote_widgets.setObjectName("quote_widgets")
+        self.quote_image = QtWidgets.QLabel(self.quote_widgets)
+        self.quote_image.setGeometry(QtCore.QRect(150, 35, 450, 300))
+        self.quote_image.setStyleSheet("border-radius : 0px;\n"
+"background-image: url(./image/quote_1.jpg);\n"
+"border : None;\n"
+"background-size: cover;")
+        self.quote_image.setText("")
+        self.quote_image.setObjectName("quote_image")
+
         self.tabWidget.addTab(self.dash_board, "")
         self.profile = QtWidgets.QWidget()
         self.profile.setStyleSheet("background : qlineargradient(x2:1, y2:1, x0:1, y0:1,\n"
@@ -116,7 +135,7 @@ class Ui_main_page(object):
         self.profile_information_label.setStyleSheet("border : none;")
         self.profile_information_label.setObjectName("profile_information_label")
         self.profile_picture = QtWidgets.QLabel(self.profile_widget_1)
-        self.profile_picture.setGeometry(QtCore.QRect(50, 150, 51, 51))
+        self.profile_picture.setGeometry(QtCore.QRect(50, 160, 51, 51))
         font = QtGui.QFont()
         font.setFamily("Font Awesome 5 Free Regular")
         font.setPointSize(30)
@@ -132,17 +151,23 @@ class Ui_main_page(object):
         self.name_label.setStyleSheet("border : none;\n"
 "")
         self.name_label.setObjectName("name_label")
+        
+        #NAME
         self.name_lineEdit = QtWidgets.QLineEdit(self.profile_widget_1)
-        self.name_lineEdit.setGeometry(QtCore.QRect(140, 170, 300, 30))
+
+        #SET_DEFAULT_NAME
+        self.name_lineEdit.setText("default_name")
+        self.name_lineEdit.setGeometry(QtCore.QRect(140, 170, 380, 41))
         font = QtGui.QFont()
         font.setFamily("Segoe UI Light")
-        font.setPointSize(18)
+        font.setPointSize(16)
         self.name_lineEdit.setFont(font)
         self.name_lineEdit.setStyleSheet("border-radius : 0px;\n"
 "border-style : groove;\n"
 "border-width : 1.5px;\n"
 "border-color : rgb(128,128,128);")
         self.name_lineEdit.setObjectName("name_lineEdit")
+
         self.email_label = QtWidgets.QLabel(self.profile_widget_1)
         self.email_label.setGeometry(QtCore.QRect(140, 260, 81, 31))
         font = QtGui.QFont()
@@ -152,25 +177,32 @@ class Ui_main_page(object):
         self.email_label.setStyleSheet("border : none;\n"
 "")
         self.email_label.setObjectName("email_label")
+
+        #EMAIL
         self.email_lineEdit = QtWidgets.QLineEdit(self.profile_widget_1)
-        self.email_lineEdit.setGeometry(QtCore.QRect(140, 310, 300, 30))
+        self.email_lineEdit.setGeometry(QtCore.QRect(140, 310, 380, 41))
         font = QtGui.QFont()
         font.setFamily("Segoe UI Light")
-        font.setPointSize(18)
+        font.setPointSize(16)
         self.email_lineEdit.setFont(font)
+
+        #SET_DEFAULT_EMAIL
+        self.email_lineEdit.setText("default_email")
+        
         self.email_lineEdit.setStyleSheet("border-radius : 0px;\n"
 "border-style : groove;\n"
 "border-width : 1.5px;\n"
 "border-color : rgb(128,128,128);")
         self.email_lineEdit.setObjectName("email_lineEdit")
         self.email_picture = QtWidgets.QLabel(self.profile_widget_1)
-        self.email_picture.setGeometry(QtCore.QRect(50, 297, 51, 51))
+        self.email_picture.setGeometry(QtCore.QRect(50, 310, 51, 51))
         font = QtGui.QFont()
         font.setFamily("Font Awesome 5 Free Regular")
         font.setPointSize(30)
         self.email_picture.setFont(font)
         self.email_picture.setStyleSheet("border : none;")
         self.email_picture.setObjectName("email_picture")
+
         self.password_label = QtWidgets.QLabel(self.profile_widget_1)
         self.password_label.setGeometry(QtCore.QRect(140, 400, 141, 31))
         font = QtGui.QFont()
@@ -180,11 +212,16 @@ class Ui_main_page(object):
         self.password_label.setStyleSheet("border : none;\n"
 "")
         self.password_label.setObjectName("password_label")
+
+        #PASSWORD
         self.password_lineEdit = QtWidgets.QLineEdit(self.profile_widget_1)
-        self.password_lineEdit.setGeometry(QtCore.QRect(140, 450, 300, 30))
+        #SET_DEFAULT_PASSWORD
+        self.password_lineEdit.setText("default_password")
+
+        self.password_lineEdit.setGeometry(QtCore.QRect(140, 450, 380, 41))
         font = QtGui.QFont()
         font.setFamily("Segoe UI Light")
-        font.setPointSize(18)
+        font.setPointSize(16)
         self.password_lineEdit.setFont(font)
         self.password_lineEdit.setStyleSheet("border-radius : 0px;\n"
 "border-style : groove;\n"
@@ -192,7 +229,7 @@ class Ui_main_page(object):
 "border-color : rgb(128,128,128);")
         self.password_lineEdit.setObjectName("password_lineEdit")
         self.password_picture = QtWidgets.QLabel(self.profile_widget_1)
-        self.password_picture.setGeometry(QtCore.QRect(50, 430, 61, 51))
+        self.password_picture.setGeometry(QtCore.QRect(50, 440, 61, 51))
         font = QtGui.QFont()
         font.setFamily("Font Awesome 5 Free Regular")
         font.setPointSize(30)
@@ -205,6 +242,7 @@ class Ui_main_page(object):
 "border-radius : 15px;\n"
 "background : white;")
         self.profile_widget_2.setObjectName("profile_widget_2")
+
         self.birth_date_label = QtWidgets.QLabel(self.profile_widget_2)
         self.birth_date_label.setGeometry(QtCore.QRect(140, 100, 171, 31))
         font = QtGui.QFont()
@@ -214,17 +252,7 @@ class Ui_main_page(object):
         self.birth_date_label.setStyleSheet("border : none;\n"
 "")
         self.birth_date_label.setObjectName("birth_date_label")
-        self.birth_date_lineEdit = QtWidgets.QLineEdit(self.profile_widget_2)
-        self.birth_date_lineEdit.setGeometry(QtCore.QRect(140, 160, 300, 30))
-        font = QtGui.QFont()
-        font.setFamily("Segoe UI Light")
-        font.setPointSize(18)
-        self.birth_date_lineEdit.setFont(font)
-        self.birth_date_lineEdit.setStyleSheet("border-radius : 0px;\n"
-"border-style : groove;\n"
-"border-width : 1.5px;\n"
-"border-color : rgb(128,128,128);")
-        self.birth_date_lineEdit.setObjectName("birth_date_lineEdit")
+
         self.weight_label = QtWidgets.QLabel(self.profile_widget_2)
         self.weight_label.setGeometry(QtCore.QRect(140, 260, 101, 31))
         font = QtGui.QFont()
@@ -234,7 +262,13 @@ class Ui_main_page(object):
         self.weight_label.setStyleSheet("border : none;\n"
 "")
         self.weight_label.setObjectName("weight_label")
+
+         #WEIGHT
         self.weight_lineEdit = QtWidgets.QLineEdit(self.profile_widget_2)
+        
+        #SET_DEFAULT_WEIGHT
+        self.weight_lineEdit.setText("999")
+        
         self.weight_lineEdit.setGeometry(QtCore.QRect(140, 310, 100, 30))
         font = QtGui.QFont()
         font.setFamily("Segoe UI Light")
@@ -244,9 +278,10 @@ class Ui_main_page(object):
 "border-style : groove;\n"
 "border-width : 1.5px;\n"
 "border-color : rgb(128,128,128);")
+        self.weight_lineEdit.setAlignment(QtCore.Qt.AlignCenter)
         self.weight_lineEdit.setObjectName("weight_lineEdit")
         self.birth_date_picture = QtWidgets.QLabel(self.profile_widget_2)
-        self.birth_date_picture.setGeometry(QtCore.QRect(50, 137, 51, 51))
+        self.birth_date_picture.setGeometry(QtCore.QRect(50, 150, 51, 51))
         font = QtGui.QFont()
         font.setFamily("Font Awesome 5 Free Regular")
         font.setPointSize(30)
@@ -270,7 +305,13 @@ class Ui_main_page(object):
         self.height_label.setStyleSheet("border : none;\n"
 "")
         self.height_label.setObjectName("height_label")
+
+        #HEIGHT
         self.height_lineEdit = QtWidgets.QLineEdit(self.profile_widget_2)
+
+        #SET_DEFAULT_HEIGHT
+        self.height_lineEdit.setText("999")
+
         self.height_lineEdit.setGeometry(QtCore.QRect(370, 310, 100, 30))
         font = QtGui.QFont()
         font.setFamily("Segoe UI Light")
@@ -280,6 +321,7 @@ class Ui_main_page(object):
 "border-style : groove;\n"
 "border-width : 1.5px;\n"
 "border-color : rgb(128,128,128);")
+        self.height_lineEdit.setAlignment(QtCore.Qt.AlignCenter)
         self.height_lineEdit.setObjectName("height_lineEdit")
         self.height_picture = QtWidgets.QLabel(self.profile_widget_2)
         self.height_picture.setGeometry(QtCore.QRect(310, 290, 51, 51))
@@ -300,17 +342,6 @@ class Ui_main_page(object):
         self.gender_label.setStyleSheet("border : none;\n"
 "")
         self.gender_label.setObjectName("gender_label")
-        self.gender_lineEdit = QtWidgets.QLineEdit(self.profile_widget_2)
-        self.gender_lineEdit.setGeometry(QtCore.QRect(140, 450, 100, 30))
-        font = QtGui.QFont()
-        font.setFamily("Segoe UI Light")
-        font.setPointSize(18)
-        self.gender_lineEdit.setFont(font)
-        self.gender_lineEdit.setStyleSheet("border-radius : 0px;\n"
-"border-style : groove;\n"
-"border-width : 1.5px;\n"
-"border-color : rgb(128,128,128);")
-        self.gender_lineEdit.setObjectName("gender_lineEdit")
         self.weight_picture_2 = QtWidgets.QLabel(self.profile_widget_2)
         self.weight_picture_2.setGeometry(QtCore.QRect(50, 430, 61, 51))
         font = QtGui.QFont()
@@ -319,7 +350,10 @@ class Ui_main_page(object):
         self.weight_picture_2.setFont(font)
         self.weight_picture_2.setStyleSheet("border : none;")
         self.weight_picture_2.setObjectName("weight_picture_2")
+
+        #SAVE_BUTTON
         self.save_Button = QtWidgets.QPushButton(self.profile_widget_2)
+        self.save_Button.clicked.connect(self.save)
         self.save_Button.setGeometry(QtCore.QRect(450, 519, 141, 41))
         font = QtGui.QFont()
         font.setFamily("Yu Gothic")
@@ -331,13 +365,54 @@ class Ui_main_page(object):
 "border-radius : 20px;\n"
 "")
         self.save_Button.setObjectName("save_Button")
+
+        #BIRTH_DATE
+        self.birth_dateEdit = QtWidgets.QDateEdit(self.profile_widget_2)
+
+        #SET_DEFAULT_BIRTH_DATE (Format : yyy , mm, dd)
+        self.birth_dateEdit.setDate(QtCore.QDate(1999, 12, 28))
+
+        self.birth_dateEdit.setGeometry(QtCore.QRect(139, 160, 301, 35))
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI")
+        font.setPointSize(14)
+        self.birth_dateEdit.setFont(font)
+        self.birth_dateEdit.setStyleSheet("border : black;\n"
+"border-style: solid;\n"
+"border-width: 1px;\n"
+"border-radius : 0px;\n"
+"border-color : rgb(128,128,128);")
+        self.birth_dateEdit.setCalendarPopup(True)
+        self.birth_dateEdit.setObjectName("birth_dateEdit")
+
+        #GENDER
+        self.sex_comboBox = QtWidgets.QComboBox(self.profile_widget_2)
+        
+        #SET_DEFAULT_GENDER (MALE == 0)
+        self.sex_comboBox.setCurrentIndex(0)
+
+        self.sex_comboBox.addItem("Male")
+        self.sex_comboBox.addItem("Female")
+        self.sex_comboBox.addItem("Other")
+        self.sex_comboBox.setGeometry(QtCore.QRect(140, 450, 151, 31))
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI Light")
+        font.setPointSize(16)
+        self.sex_comboBox.setFont(font)
+        self.sex_comboBox.setStyleSheet("border : black;\n"
+"border-style: solid;\n"
+"border-width: 1px;\n"
+"border-radius : 0px;\n"
+"border-color : rgb(128,128,128);")
+        self.sex_comboBox.setEditable(True)
+        self.sex_comboBox.setObjectName("sex_comboBox")
         self.tabWidget.addTab(self.profile, "")
         self.user_name = QtWidgets.QWidget()
         self.user_name.setObjectName("user_name")
         self.tabWidget.addTab(self.user_name, "")
 
         self.retranslateUi(main_page)
-        self.tabWidget.setCurrentIndex(0)
+        self.tabWidget.setCurrentIndex(1)
         QtCore.QMetaObject.connectSlotsByName(main_page)
 
     def retranslateUi(self, main_page):
@@ -358,15 +433,17 @@ class Ui_main_page(object):
         self.password_picture.setText(QtWidgets.QApplication.translate("main_page", "", None, -1))
         self.birth_date_label.setText(QtWidgets.QApplication.translate("main_page", "Birth-date", None, -1))
         self.weight_label.setText(QtWidgets.QApplication.translate("main_page", "Weight", None, -1))
+        self.weight_lineEdit.setInputMask(QtWidgets.QApplication.translate("main_page", "999", None, -1))
         self.birth_date_picture.setText(QtWidgets.QApplication.translate("main_page", "", None, -1))
         self.weight_picture.setText(QtWidgets.QApplication.translate("main_page", "", None, -1))
         self.height_label.setText(QtWidgets.QApplication.translate("main_page", "Height", None, -1))
+        self.height_lineEdit.setInputMask(QtWidgets.QApplication.translate("main_page", "999", None, -1))
         self.height_picture.setText(QtWidgets.QApplication.translate("main_page", "", None, -1))
         self.gender_label.setText(QtWidgets.QApplication.translate("main_page", "Gender", None, -1))
         self.weight_picture_2.setText(QtWidgets.QApplication.translate("main_page", "", None, -1))
         self.save_Button.setText(QtWidgets.QApplication.translate("main_page", "Save", None, -1))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.profile), QtWidgets.QApplication.translate("main_page", "PROFILE", None, -1))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.user_name), QtWidgets.QApplication.translate("main_page", "                                                                                                        Sarin Wanichwasin", None, -1))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.user_name), QtWidgets.QApplication.translate("main_page", "                                                                                                        " + self.name_lineEdit.text(), None, -1))
 
 # from mplwidget import MplWidget
 
